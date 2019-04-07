21919de2ed62d56eaef5ad3714ef97e73d3d48e5
AaronCedillo <adrian_sonny55@hotmail.com>
2019-04-06 23:27:52 -0600

    Factorial of any number

---
+++ Factorial.cpp
@@ -0,0 +1,31 @@
+// Crear un programa el cual nos pida un valor de entrada, a este valor de entrada se le debe sacar su factorial.
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int x;
+int fact;
+
+void Factorial() {
+
+	fact = x;
+	
+	for(int i = x - 1; i >= 1; i--) {
+		
+		fact = fact * i;
+	}
+	
+	cout << endl;
+	cout << "El factorial de " << x << " es: " << fact;
+}
+
+int main() {
+	
+	cout << "Ingresa un numero: ";
+	cin >> x;
+	
+	Factorial();
+}
+
