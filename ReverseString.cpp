bda5a480b35372acb9aeed64c6472de36d99aac9
AaronCedillo <adrian_sonny55@hotmail.com>
2019-04-06 23:52:34 -0600

    Take a string and gives you the reverse string

---
+++ ReverseString.cpp
@@ -0,0 +1,25 @@
+// Crear un programa el cual sea capaz de revisir un string, este string debera ser impreso en forma inversa 
+	// Ejemplo: "hola" , out = "aloh".
+	
+#include <iostream>	
+#include <stdlib.h>
+
+using namespace std;
+
+string input;
+
+void String() {
+	
+	for(int i = input.length(); i <= input.length(); i--) {
+		
+		cout << input[i];
+	}
+}
+
+int main() {
+	
+	cout << "Dame una cadena de caracteres: ";
+	cin >> input;
+	
+	String();
+}
