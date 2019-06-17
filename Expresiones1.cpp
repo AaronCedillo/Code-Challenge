9357ec6d9ff383b045e0dba76b0b14e176561234
AaronCedillo <adrian_sonny55@hotmail.com>
2019-06-17 17:28:57 -0500

    test

--- Expresiones1.cpp
+++ Expresiones1.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 
 using namespace std;
-
-	float a, b, c, d, resultado;
+int x;
+void myfunc() {
 	
-void Expresiones() {
-	
-	cout << "Deme 4 numeros: \n";
-	cout << "a: ";
-	cin >> a;
-	cout << "b: ";
-	cin >> b;
-	cout << "c: ";
-	cin >> c;
-	cout << "d: ";
-	cin >> d;
+	x*x; 
 }
 
-int main() {
-	
-	Expresiones();
-	resultado = (a + b)/(c + d);
-	cout << "El resultado es: " << resultado;
-}
+void main() 
+{ 
+  int i; 
+  i = 16/myfunc(2); 
+  printf("%d",i); 
+} 
+
