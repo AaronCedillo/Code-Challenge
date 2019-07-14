b1a2c1a1f9f62bc201ec23732c0ed84186dec1e6
AaronCedillo <adrian_sonny55@hotmail.com>
2019-07-13 20:01:32 -0500

    Punteros basicos por referencia

---
+++ Punteros1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int funcion(int valor) {
+	
+	valor = valor + 10;
+	return valor;	
+}
+
+int funcionPunteros(int *valor) {
+	
+	*valor = *valor + 10;
+	return *valor;
+}
+
+int main() {
+	
+	int numero = 10;	
+	cout << "Al inicio de la funcion: " << numero << "\n";
+	
+	funcion(numero);	
+	cout << "Despues de llavamar a la funcion: " << numero << "\n";
+	
+	funcionPunteros(&numero);
+	cout << "Despues de la funcion punetros: " << numero << "\n";
+}
